2519bab7768727e76fa7ca8aba2959e8217ad527
HungLe <hungld.nta@gmail.com>
2022-04-26 21:09:06 +0700

    First commit

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,42 @@
+const { hexStripZeros } = require("ethers/lib/utils");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
+
+async function main() {
+    const randomWinnerGame = await ethers.getContractFactory("RandomWinnerGame");
+    const deployedRandomWinnerGameContract = await randomWinnerGame.deploy(
+        VRF_COORDINATOR,
+        LINK_TOKEN,
+        KEY_HASH,
+        FEE,
+    );
+
+    await deployedRandomWinnerGameContract.deployed();
+
+    console.log(
+        "Verify Contract Address",
+        deployedRandomWinnerGameContract.address
+    );
+
+    console.log("Sleeping ...");
+    await sleep(30000);
+
+    // Verify the contract after the deploying
+    await hre.run("verify:verify", {
+        address: deployedRandomWinnerGameContract.address,
+        constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+    });
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
